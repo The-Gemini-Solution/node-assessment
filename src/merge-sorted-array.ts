8fde13756737903d4d21b50e865c655ef2d2525d
Lee <leevashan.reddi@aax.co.za>
2021-06-28 19:40:56 +0200

    boom

--- src/merge-sorted-array.ts
+++ src/merge-sorted-array.ts
@@ -1,3 +1,9 @@
 export function mergeSortedArray(nums1: number[], m: number, nums2: number[], n: number){
-    return true;
+    if (n === 0) return;
+
+    for (let i = 0; i < nums2.length; i++) {
+        nums1[i + m] = nums2[i]; 
+    }
+
+    nums1.sort((a,b) =>  a - b);
 }
